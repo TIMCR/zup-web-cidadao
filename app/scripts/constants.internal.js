ee27ecdeb43c02316a12d5224b52f24a705af3dc
João Paulo Polles <joaopaulopolles@gmail.com>
2017-10-10 14:50:35 -0300

    alterações necessárias

---
+++ app/scripts/constants.internal.js
@@ -0,0 +1,7 @@
+"use strict";
+
+ angular.module('config', [])
+
+.constant('ENV', {name:'development',apiEndpoint:'http://192.168.1.34:8282',mapLat:'-24.5581316',mapLng:'-54.057559',mapZoom:'14'})
+
+;
